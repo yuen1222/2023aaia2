eea4eddcca26ade182d81f5b70603ff2492543d7
yuen1222 <11160242@me.mcu.edu.tw>
2024-03-20 11:06:00 +0800

    week05

---
+++ week05/week05-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0;i<10;i++){
+        printf("i¬O%d",i);
+    }
+    printf("\n");
+    for(int i=1;i<=9;i++){
+        printf("i¬O%d",i);
+    }
+}
