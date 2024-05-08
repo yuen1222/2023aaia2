c725e44e9d80be4fa7e9d8620f16deb68126d023
unknown <11160242@me.mcu.edu.tw>
2024-05-08 11:47:40 +0800

    week12

---
+++ week12/week12-1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int isPrime(int n)
+{
+    for(int i=2;i<n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    printf("請輸入1個數:");
+    int n;
+    scanf("%d",&n);
+    if(isPrime(n)) printf("n是質數");
+    else printf("n不是質數");
+}
---
+++ week12/week12-2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int isPrime(int n)
+{
+    for(int i=2;i<n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    printf("請輸入1個數:");
+    int n;
+    scanf("%d",&n);
+    for(int i=2;i<=n;i++){
+        if(isPrime(i)) printf("%d ",i);
+    }
+}
