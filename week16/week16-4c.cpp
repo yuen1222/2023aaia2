111c981589a87826ef19da6303daf940c6fc7894
yuen1222 <114201608+yuen1222@users.noreply.github.com>
2024-06-05 11:25:43 +0800

    Add files via upload

---
+++ week16/week16-4c.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char line[100];
+	scanf("%s",line);
+	for(int i=0;line[i]!=0;i++){
+		if(line[i]>='0'&&line[i]<='9'){
+			printf("%c\n",line[i]);
+			break;
+		}
+	}
+}
